4356e0557310ce4c0ebdb80edae91e5964f65d0a
rahulmalviya321 <154511988+rahulmalviya321@users.noreply.github.com>
2024-01-19 00:51:03 -0800

    file upload

---
+++ 18jan 2bech string function.py
@@ -0,0 +1,39 @@
+# a= "TheRahul"
+# print(a.lower())
+ 
+x="raviraj"
+print(x.upper())
+
+y="rajkumar"
+print(y.swapcase())
+
+z="akshay"
+print(z.title())
+
+#find character
+x="arihant"
+print(x.count("a"))
+
+#which number find
+y="australia"
+print(y.find("t")) 
+
+#back find
+x="british"
+print(y.rfind("s"))
+
+y="priyanshu"
+print(y.find("n"))
+
+#removewhitespace
+x="star is twincle"
+print(x.split())
+#coma space
+
+y="star is not raining"
+print(y.lstrip())
+
+x="mens are running"
+# print(x.replace("g","m"))
+h=x.replace("g","m")
+print(h)
